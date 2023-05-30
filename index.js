48cd5d34a6af586985ddd3060316259f42f1be5f
Karunesh Anand <83410805+KARUNESHANAND@users.noreply.github.com>
2023-05-30 22:18:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+var mySuperheroName = superheroes.random();
+var mySuperVillainName = supervillains.random();
+console.log("This is a super hero name : " + mySuperheroName);
+console.log("This is a super villain name : " + mySuperVillainName);
